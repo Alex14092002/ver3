61fed839aed4222248519aae76a41ad3ff1b84e2
DEV PHU TUAN <2051010343tuan@ou.edu.vn>
2023-03-23 00:39:13 +0700

    update

--- scroll.js
+++ scroll.js
@@ -1,6 +1,6 @@
 const boxSp = document.getElementById('box-sticky')
 
-
+const list =document.getElementById('list')
 
 
 
@@ -10,11 +10,15 @@ const boxSp = document.getElementById('box-sticky')
 window.addEventListener("scroll", function() {
     const boxSticky = document.getElementById('box-sp-sticky')
     var scrollPosition = window.scrollY;
-    console.log(scrollPosition);
+   
     if (scrollPosition > 800) {
+        boxSp.classList.add('hidden')
         boxSticky.classList.remove("hidden");
+        list.classList.remove('hidden')
     } else {
       boxSticky.classList.add("hidden");
+      list.classList.add('hidden')
+      boxSp.classList.remove('hidden')
     }
   });
   
